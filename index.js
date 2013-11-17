1919a45f4c39ee783400116ac7fc6c5e26bf8817
jesse keane <me@jessekeane.me>
2013-11-16 19:04:48 -0800

    first test

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 var http = require('http'),
-    router = require('route-emitter'),
-    port = process.env.WENDIGO_PORT || 7654
+    router = require('route-emitter').createRouter(),
+    request = require('hyperquest'),
+    port = process.env.WENDIGO_PORT || 7654,
+    bigfoot = process.env.BIGFOOT || 'http://localhost:8165',
+    sasquatch = process.env.SASQUATCH || 'http://localhost:5450',
+    yeren = process.env.YEREN || 'http://localhost:7353',
+    yowie = process.env.YOWIE || 'http://localhost:7031'
 
-http.createServer().listen(3000)
+router.listen('*', 'docs', function (req, res) {
+  var method = req.method.toLowerCase()
+  req.pipe(request[method](bigfoot)).pipe(request[method](sasquatch))
+})
+
+router.listen('*', 'users', function (req, res) {
+  var method = req.method.toLowerCase()
+  req.pipe(request[method](bigfoot)).pipe(request[method](yeren))
+})
+
+router.listen('*', 'stats', function (req, res) {
+  var method = req.method.toLowerCase()
+  req.pipe(request[method](bigfoot)).pipe(request[method](yowie))
+})
+
+http.createServer(router.route).listen(port)
